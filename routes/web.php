29f9bd58474385d65f53258f10f8f325b1491f7e
Ahmadk-usman <ahmadkurniawan.s.tr.t@gmail.com>
2025-02-20 14:24:40 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [AuthController::class, 'loginpage'])->name('login');
 Route::post('/login', [AuthController::class, 'login'])->name('login.submit');
+Route::post('/api/login', [TaskController::class, 'login'])->name('login.submit');
+
 
 Route::middleware(['auth'])->group(function () {
     Route::get('/dashboard', [AuthController::class, 'dashboard'])->name('dashboard');
